6030cf56464bf446c5c2f81c637ac7cfecd84ff0
Daniel Fuentes <=>
2020-10-22 20:09:20 -0300

    trabajando con sequelize

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,9 +1,18 @@
 const db = require('../database/models');
+const path = require('path');
 
 const moviesController = {
 
     listar : function(req,res){
-        return res.send('Hola como vamos.... ');
+        //return res.send('Hola como vamos.... ');
+        //       select * from movies 
+        db.Movie.findAll()
+        .then(peliculas =>{
+            //return res.send(peliculas[0].title)
+            res.render(path.resolve(__dirname, '../views/listarPeliculas'), {peliculas})
+        })
+        .catch(error => res.send(error))
+
     }
 
 }
--- src/database/models/movie.js
+++ src/database/models/movie.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, dataTypes) =>{
-    let alias = 'Movies';
+    let alias = 'Movie';
     let cols = {
         id : {
             type: dataTypes.INTEGER,
